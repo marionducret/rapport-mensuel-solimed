22763dda27b26b659422dc25e4012823b367fc21
marionducret <70635718+marionducret@users.noreply.github.com>
2026-03-19 17:03:00 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import core
+
+st.set_page_config(layout="wide")
+
+st.title("Générateur de rapport")
+
+comments = {}
+
+figures = core.generate_all_figures()
+
+for theme, fig, plots in figures:
+    st.header(theme)
+
+    col1, col2 = st.columns([2, 1])  # ← ICI on crée les colonnes
+
+    with col1:
+        st.pyplot(fig)  # graphe à gauche
+
+    with col2:
+        for col, titre in plots:
+            auto_comment = core.generate_comment(col, titre)
+
+            edited = st.text_area(
+                f"{titre}",
+                value=auto_comment,
+                height=120
+            )
+
+            comments[(theme, col)] = edited
+
+    st.divider()
+
+# bouton final
+if st.button("Générer le PDF"):
+    core.generate_pdf(custom_comments=comments)
+    st.success("PDF généré !")
