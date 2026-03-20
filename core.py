3dc1d72bc066b134ee2d40ae98cd24e8aebac47a
marionducret <70635718+marionducret@users.noreply.github.com>
2026-03-20 11:18:16 +0100

    Add files via upload

--- core.py
+++ core.py
@@ -295,7 +295,7 @@ def load_data(uploaded_zip, uploaded_excel):
     evol_df = evol_df.reset_index()
     evol_df["jour_tot_supp"] = 0
 
-    evol_df = evol_df[evol_df["Mois"] != MOIS_EXCLUS]#supp 2026 
+    evol_df = evol_df[~evol_df["Mois"].isin(MOIS_EXCLUS)]#supp 2026
 
     PERIODE = f"{evol_df['Mois'].iloc[0]} → {evol_df['Mois'].iloc[-1]}"
 
