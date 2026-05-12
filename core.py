38d20d7a9b482897a7b0e717147f31911bcf6e2c
Marion Ducret <marion.ducret@ymail.com>
2026-05-12 15:43:34 +0200

    comm length

--- core.py
+++ core.py
@@ -1054,8 +1054,8 @@ PAGE_NUM_Y    = 0.020
 PAGE_NUM_X    = 0.970
 
 # Largeur maximale des commentaires, en nombre de caractères par ligne.
-COMMENT_SMALL_MAX_CHARS = 100
-COMMENT_BIG_MAX_CHARS = 60
+COMMENT_SMALL_MAX_CHARS = 95
+COMMENT_BIG_MAX_CHARS = 70
 
 # Position du début du texte dans chaque bloc commentaire.
 COMMENT_SMALL_TEXT_X = 0.025
